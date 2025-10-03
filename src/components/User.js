34f9604efae3b317d882913e7c63896acc0d0e52
AlketaBala <93828279+AlketaBala@users.noreply.github.com>
2025-10-03 22:33:40 +0200

    Added the styling Part of homepage

--- src/components/User.js
+++ src/components/User.js
@@ -4,14 +4,11 @@ const User = ({id, email, name, company}) => {
 
 
     return ( //User name, email, and company name and button  for navigation to user details
-        <div className='list'>
-         <span>{name}</span> 
-         <span>{email}</span>
-         <span>{company}</span>
-         <span>
-            <button>Click here to see more details</button> 
-         </span>
-        
+        <div className='user-card'>
+         <h3>{name}</h3>
+         <p><strong>{email}</strong></p>
+         <p><strong>{company}</strong></p>
+         
         </div>
     )
 }
