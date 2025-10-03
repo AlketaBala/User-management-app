2a8059a4693d742100cc43eee305bb53c60d7bab
AlketaBala <93828279+AlketaBala@users.noreply.github.com>
2025-10-03 22:00:10 +0200

    Added a UserDetailsPage.css file where the styling part of User Details Page is

--- src/pages/UserDetailsPage.js
+++ src/pages/UserDetailsPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Link ,useLocation, useParams} from 'react-router-dom';
-
+import './UserDetailsPage.css'
 const UserDetailsPage =()=> {
 
 
@@ -13,17 +13,18 @@ if(!user){
 }// Here its all the details of the user that will be shown 
 // A button that you can click to get back to the homepage
     return (
-        <div>
+        <div className="container">
             <h1>User Details Page</h1>
+            <div className='details'>
             <p>User ID: {id}</p>
             <p>Name: {user.name}</p>
             <p>Phone: {user.phone}</p>
             <p>Website: {user.website}</p>
             <p>Address: {user.address.street}, {user.address.suite}, {user.address.zipcode},{user.address.city}</p>
-           
+           </div>
       
         <Link to="/">
-            <button>Back to home</button> 
+            <button className='back-button'>Back to home</button> 
         </Link>
           </div>
     )
