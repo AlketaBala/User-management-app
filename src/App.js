00dcb801356414dcd952a7b96197022bbb5f35fa
AlketaBala <93828279+AlketaBala@users.noreply.github.com>
2025-10-03 17:37:14 +0200

    Added the Add User button for adding new Users

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import User from './components/User' //Importing the User component to use here
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import UserDetailsPage from './pages/UserDetailsPage';
 import logo from './logo.svg';
+import addUserPage from './pages/addUserPage';
 
 
 
@@ -32,6 +33,7 @@ const App = () => { //Create the state variable users to store the list that is 
     user.email.toLowerCase().includes(input.toLowerCase()) // or by email
    );
 
+  
    return (
     <BrowserRouter>
     <Routes>
@@ -47,15 +49,24 @@ const App = () => { //Create the state variable users to store the list that is 
       {
        FilterUsers.map((user) => ( // Here is the display of users that we search for
         // users id, name, email and company name
+        //When clicking a user through this link we can navigate to Details Page about that user
         <Link to={`/user/${user.id}`} key={user.id} state={{user}}>
         <User id={user.id} key={user.id} name= {user.name} email={user.email} company={user.company.name} /> 
        </Link>
        ))
       }
      </div>
+      <div>
+        <Link to="/addUser">
+         <button> Add User </button>
+         </Link>
+      </div>
+
+
     </div>
       }
       />
+      <Route path="/addUser" element ={<addUserPage/>} />
       <Route path="/user/:id" element={<UserDetailsPage/>} />
     </Routes>
     </BrowserRouter>
